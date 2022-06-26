5af7904bcb87daf28af1a8c29a94f8d36cced9b2
dlord-20 <dthomaslord@gmail.com>
2022-06-26 20:39:01 +0300

    Testimonials are now changing correctly execept the bug when you switch pages and it still has the preivous testimony that shouldn't be on the new page

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import {
+    React,
+    useEffect,
+    useState
+    } from 'react';
+import { 
+    NavLink, 
+    Outlet, 
+    } from 'react-router-dom';
 import danceClasses from './testimonial-object';
 import Testimony from './testimony';
 
@@ -14,17 +21,41 @@ export default function Testimonials(props) {
         console.log('here');
     }
 
-    const display = filteredTestimonies.map(item => <Testimony testimony={item}/>)
+    const display = filteredTestimonies.map(item => <Testimony testimony={item}/>);
 
+    const [displayIndex, setDisplayIndex] = useState(0);
+    const [displayTestimony, setDisplayTestimony] = useState(display[displayIndex]);
+
+    //Need a way to call this when we change a page
+    const changeDisplay = () => {
+
+        if(displayIndex !== (display.length - 1)) {
+            setDisplayIndex(displayIndex + 1);
+        } else {
+            setDisplayIndex(0);
+        }
+        return setDisplayTestimony(display[displayIndex]);
+    }
+
+    //set interval for changeDisplay and maybe this is how you'd fix page change
+    useEffect(() => {
+        
+        console.log(displayTestimony)
+        return () => {
+        }
+    }, [displayTestimony]);
+
+    //This is nuts
 
     return (
         <div>
             <div className="testimonies">
+                <p onClick={changeDisplay}>&#x2190;</p>
                 <div>
-                    {display}
+                    {displayTestimony}
                 </div>
+                <p onClick={changeDisplay}>&#x2192;</p>
             </div>
-            <Outlet />
         </div>
     );
   }
